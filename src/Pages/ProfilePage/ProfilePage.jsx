68ad5aa57e48a3790e3dd8e2bb82094659e1a330
JohnnyParra <juanparra1996@gmail.com>
2023-02-22 10:33:34 -0800

    updating profile page

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -7,26 +7,38 @@ import './ProfilePage.css'
 
 
 export default function Profile() {
+  const [input, setInput] = useState({avatar: ''})
   const [previewImage, setPreviewImage] = useState('');
   const { currentUser } = useContext(UserContext);
   function avatarChange(event){
+    setInput(prevInput => ({...prevInput, [event.target.name]: event.target.files[0]}))
     setPreviewImage(URL.createObjectURL(event.target.files[0]));
   }
+
   return(
     <main className="profile-page">
       <div className="App">
         <Navbar />
         <div className="avatar">
-          <input accept="image/*" id="avatar-pic" onChange={(event) => avatarChange(event)}type='file' hidden></input>
+          <input accept="image/*" id="avatar-pic" name="avatar" onChange={(event) => avatarChange(event)}type='file' hidden></input>
           <label htmlFor="avatar-pic">
             <IconButton component="span">
               <Avatar src={previewImage} sx={{ fontSize: 100, width: 200, height: 200, m: 1, bgcolor: '#ff3d00' }}>{currentUser.userInfo[0]?.name[0]}</Avatar>
             </IconButton>
           </label>
         </div>
+        {input.avatar && <button>Save</button>}
         <p>Member since: {new Date(Number(currentUser.userInfo[0].date_created)).toLocaleString()}</p>
-        <input type="text" defaultValue={currentUser.userInfo[0].name}></input>
-        <input type="text" defaultValue={currentUser.userInfo[0].email}></input>
+        <div className="input-container">
+          <div className="username">
+            <p>UserName: </p>
+            <input type="text" defaultValue={currentUser.userInfo[0].name}></input>
+          </div>
+          <div className="email">
+            <p>Email: </p>
+          <input type="text" defaultValue={currentUser.userInfo[0].email}></input>
+          </div>
+        </div>
       </div>
     </main>
   )
