43c0e34a620d4f0261b7d757bef4df4d65890b90
Johnny Parra <juanparra1996@gmail.com>
2024-07-23 17:14:28 -0700

    API url fix

--- src/Environment/environment.dev.jsx
+++ src/Environment/environment.dev.jsx
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000';
-// export const API_URL = 'https://blog-website-server-plum.vercel.app';
+// export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://blog-website-server-plum.vercel.app';
